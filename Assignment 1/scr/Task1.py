53138f3df69eaca9618f52389fd509c4dfb647a9
AliJnadi <90157234+AliJnadi@users.noreply.github.com>
2021-09-12 09:22:38 +0300

    final

--- Assignment 1/scr/Task1.py
+++ Assignment 1/scr/Task1.py
@@ -4,13 +4,12 @@ import io
 
 
 def decorator1(original_function):
-    def wrapper(*arg, **argn):
+    def wrapper(*arg, **kwargs):
         wrapper.count += 1
-        with redirect_stdout(io.StringIO()) as f:
+        with redirect_stdout(io.StringIO()) as _:
             start = time.perf_counter()
-            result = original_function(*arg, **argn)
+            result = original_function(*arg, **kwargs)
             end = time.perf_counter()
-        s = f.getvalue()
         print(f'{original_function.__name__:<10}\tcall {wrapper.count} executed in{end - start: .4f} sec')
         return result
     wrapper.count = 0
